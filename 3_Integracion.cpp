33d6b9d40567b281863084e3f7552bbcb88fdb67
mllangon <mllangon@myuax.com>
2023-11-23 12:24:35 +0100

    3

---
+++ 3_Integracion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string cadena1, cadena2;
+
+    cout << "Introduce la pimera frase: ";
+    getline(cin, cadena1);
+    cout<< "Introduce lla segunda parte de la frase: ";
+    getline(cin, cadena2);
+
+    string cadenaConcatenada = cadena1 + cadena2;
+    cout<< " la frase la completo es: " << cadenaConcatenada << endl;
+    return 0;
+}

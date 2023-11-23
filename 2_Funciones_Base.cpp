4a29fa75868f012e935befa4babcb10845571fb6
mllangon <mllangon@myuax.com>
2023-11-23 12:16:47 +0100

    1

--- 2_Funciones_Base.cpp
+++ 2_Funciones_Base.cpp
@@ -2,5 +2,18 @@
 #include<string>
 
 int main(){
+    std::string frase;
+    std::cout << "Introduce una frase: ";
+    std::getline(std::cin, frase);
+    std::cout << "La frase introducida es: " << frase << std::endl;
 
+    std::cout << "Tamano de la frase: " << frase.size() << std::endl;
+
+    if (frase.empty()) {
+        std::cout << "La cadena esta vacia." << std::endl;
+    } else {
+        std::cout << "La cadena no está vacia." << std::endl;
+    }
+
+    return 0;
 }
